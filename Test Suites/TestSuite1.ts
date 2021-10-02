b6c9bdb44408b31203db7a765fbbd3767ef209d6
rizwan <rizwan0006@gmail.com>
2021-10-03 03:42:48 +0530

    first commit

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>293bd2b7-7042-4643-a46e-e8696a29b09e</testSuiteGuid>
+   <testCaseLink>
+      <guid>35ba89ca-3b37-48f7-b906-71b2e561fa62</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a50be47d-a115-4926-ad2f-87787e35b3ab</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase2</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9ae2c642-01a6-4d73-b27b-7d8c89cc3afe</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase3</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>858e2ee6-77b6-4f10-83d2-d0898809ca65</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase4</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
